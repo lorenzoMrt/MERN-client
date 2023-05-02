1f7171f2114da0fc212eb70387faa14a7d950943
lorenzo miret <lmiret@biobam.com>
2023-05-02 23:16:18 +0200

    test

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,13 @@
 import { ICustomer } from "./Models/Customer";
 
 export function getAllCustomers() {
-    return fetch('http://localhost:8081/api/customers/all')
+    return fetch('http://172.18.64.1:8081/api/customers/all')
         .then(res => res.json())
         .then(res => res.customers);
 }
 
 export function addCustomer(customer: ICustomer): Promise<Response> {
-    return fetch('http://localhost:8081/api/customers/add', {
+    return fetch('http://172.18.64.1:8081/api/customers/add', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
