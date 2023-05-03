71f4057f305561198faa8457898f6e9e824fb007
lmiret <16326209+lorenzoMrt@users.noreply.github.com>
2023-05-03 12:29:22 +0200

    modifies port to 443(https)

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,13 @@
 import { ICustomer } from "./Models/Customer";
 
 export function getAllCustomers() {
-    return fetch('http://3.253.110.99:8081/api/customers/all')
+    return fetch('https://3.253.110.99:443/api/customers/all')
         .then(res => res.json())
         .then(res => res.customers);
 }
 
 export function addCustomer(customer: ICustomer): Promise<Response> {
-    return fetch('http://3.253.110.99:8081/api/customers/add', {
+    return fetch('https://3.253.110.99:443/api/customers/add', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
